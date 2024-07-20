9af9ebd01647db90eedd1fdab22d2b9221cbeb60
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-07-21 04:31:34 +0530

    created all controllers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,6 +10,54 @@ const createUser = async (req, res) => {
   }
 };
 
+const getAllUsers = async (req, res) => {
+  try {
+    const userData = await User.find();
+
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getOneUser = async (req, res) => {
+  try {
+    const id = await req.params.id;
+
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const id = await req.params.id;
+
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const id = await req.params.id;
+    const user = await User.findByIdAndDelete(id);
+
+    res.status(200).json({ message: "deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 module.exports = {
   createUser,
+  getAllUsers,
+  getOneUser,
+  deleteUser,
+  updateUser,
 };
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,7 +1,17 @@
 const express = require("express");
-const { createUser } = require("../controllers/userController");
+const {
+  createUser,
+  getAllUsers,
+  getOneUser,
+  deleteUser,
+  updateUser,
+} = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/create", createUser);
+router.get("/getall", getAllUsers);
+router.get("/getone/:id", getOneUser);
+router.delete("/delete/:id", deleteUser);
+router.put("/update/:id", updateUser);
 
 module.exports = router;
